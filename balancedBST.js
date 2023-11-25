04d1af948356c70a84b8079d426b26adc3d68138
gonzalo <gonzalo.a.gomez22@gmail.com>
2023-11-25 16:47:11 -0300

    build Node and Tree classes

---
+++ balancedBST.js
@@ -0,0 +1,15 @@
+class Node {
+  constructor(data) {
+    this.data = this.data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(arr) {
+    this.root = this.buildTree(arr);
+  }
+}
+
+// let array = new Tree([1, 2, 3, 4, 5, 6, 7]);
