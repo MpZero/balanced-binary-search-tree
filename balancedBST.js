4d969e8b61485a5e8c5d0addab86b17f92120b1e
gonzalo <gonzalo.a.gomez22@gmail.com>
2023-11-26 22:27:51 -0300

    create a function that takes an array and turns it into a balanced binary tree. Add function to print tree

--- balancedBST.js
+++ balancedBST.js
@@ -1,6 +1,6 @@
 class Node {
   constructor(data) {
-    this.data = this.data;
+    this.data = data;
     this.left = null;
     this.right = null;
   }
@@ -10,6 +10,34 @@ class Tree {
   constructor(arr) {
     this.root = this.buildTree(arr);
   }
+
+  buildTree(arr) {
+    let start = 0;
+    let end = arr.length - 1;
+    if (start > end) return null;
+
+    const mid = Math.ceil((start + end) / 2);
+    const root = new Node(arr[mid]);
+
+    root.left = this.buildTree(arr.slice(start, mid));
+    root.right = this.buildTree(arr.slice(mid + 1));
+
+    return root;
+  }
 }
 
-// let array = new Tree([1, 2, 3, 4, 5, 6, 7]);
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+let array = new Tree([1, 2, 3, 4, 5, 6, 7]);
+prettyPrint(array.root);
